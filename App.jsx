af6b588943b8308df92c280c9592e8dd974de2f1
maheshsolanki420440-cpu <maheshsolanki420440@gmail.com>
2025-09-04 23:54:07 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,22 +1,18 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Menu from "./components/Menu";
-import Gallery from "./components/Gallery";
-import Contact from "./components/Contact";
+import React from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Home />
       <About />
       <Menu />
-      <Gallery />
       <Contact />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
